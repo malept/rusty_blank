3c23f690fb391927a99c83f3673ec62e7634ddfd
Mark Lee <rust.ruby@lazymalevolence.com>
2016-05-03 00:10:36 -0700

    Fix dlext on OSX when using the gem

--- lib/rusty_blank.rb
+++ lib/rusty_blank.rb
@@ -1,7 +1,8 @@
 require 'fiddle'
 require 'rbconfig'
 
-basename = "librusty_blank.#{RbConfig::CONFIG['DLEXT']}"
+ext = RbConfig::CONFIG['DLEXT'] == 'bundle' ? 'dylib' : RbConfig::CONFIG['DLEXT']
+basename = "librusty_blank.#{ext}"
 library = Fiddle.dlopen(File.join(File.dirname(__FILE__), basename))
 func = Fiddle::Function.new(library['init_rusty_blank'],
                             [], Fiddle::TYPE_VOIDP)
