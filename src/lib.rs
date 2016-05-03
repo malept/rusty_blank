e3558a087cda05063e1a939700faa111408e7b47
Mark Lee <rust.ruby@lazymalevolence.com>
2016-05-03 08:36:49 -0700

    Use the methods! macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,21 @@
+#[macro_use]
 extern crate ruru;
 
-use ruru::{AnyObject, Boolean, Class, RString};
+use ruru::{AnyObject, Boolean, Class, RString, VM};
 use ruru::types::Argc;
 
-#[no_mangle]
-pub extern fn is_blank(_: Argc, _: *const AnyObject, itself: RString) -> Boolean {
-    Boolean::new(itself.to_string().chars().all(|c| c.is_whitespace()))
-}
+methods!(
+    RString,
+    itself,
+
+    fn string_is_blank() -> Boolean {
+        Boolean::new(itself.to_string().chars().all(|c| c.is_whitespace()))
+    }
+);
 
 #[no_mangle]
 pub extern fn init_rusty_blank() {
     Class::from_existing("String").define(|itself| {
-        itself.def("blank?", is_blank);
+        itself.def("blank?", string_is_blank);
     });
 }
