0ff265727069679b4855e2e987854ef4e30672df
Mark Lee <rust.ruby@lazymalevolence.com>
2016-09-25 17:15:48 -0700

    Use Thermite::Fiddle

--- lib/rusty_blank.rb
+++ lib/rusty_blank.rb
@@ -1,10 +1,6 @@
-require 'fiddle'
-require 'thermite/config'
+require 'thermite/fiddle'
 
 toplevel_dir = File.dirname(File.dirname(__FILE__))
-config = Thermite::Config.new(cargo_project_path: toplevel_dir, ruby_project_path: toplevel_dir)
-puts "Loading library: #{config.ruby_extension_path}" if config.debug_filename
-library = Fiddle.dlopen(config.ruby_extension_path)
-func = Fiddle::Function.new(library['init_rusty_blank'],
-                            [], Fiddle::TYPE_VOIDP)
-func.call
+Thermite::Fiddle.load_module('init_rusty_blank',
+                             cargo_project_path: toplevel_dir,
+                             ruby_project_path: toplevel_dir)
