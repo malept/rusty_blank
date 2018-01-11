bbe9d131c152c0852512158f41e91b548d0e4b99
Mark Lee <rust.ruby@lazymalevolence.com>
2018-01-10 19:11:42 -0800

    Add /usr/local/lib to LD_LIBRARY_PATH

--- test/ci/docker.sh
+++ test/ci/docker.sh
@@ -4,4 +4,4 @@ export PATH=$HOME/.cargo/bin:$PATH
 cd /code
 
 TRAVIS_BUILD_DIR=/code test/ci/before_install.sh
-THERMITE_DEBUG_FILENAME=/tmp/thermite.log test/ci/script.sh
+THERMITE_DEBUG_FILENAME=/tmp/thermite.log LD_LIBRARY_PATH=/usr/local/lib test/ci/script.sh
